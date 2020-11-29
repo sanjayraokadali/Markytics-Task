6daef267a5904ed936fecb1102f163b81a0ea288
sanjayraokadali <raosanjayraokadali@users.noreply.github.com>
2020-11-29 14:30:11 +0000

    Connected Postgres to Django data model

--- ReportDemo/ReportDemo/settings.py
+++ ReportDemo/ReportDemo/settings.py
@@ -76,13 +76,31 @@ WSGI_APPLICATION = 'ReportDemo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': "postgres",
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Aashu@1995',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- ReportDemo/reportApp/models.py
+++ ReportDemo/reportApp/models.py
@@ -5,25 +5,25 @@ from django.db import models
 
 class ReportIncidentModel(models.Model):
 
-    location = models.CharField(max_length=50, blank=True)
+    location = models.CharField(max_length=300, blank=True)
 
     incident_department = models.TextField(max_length=300, blank=True)
 
-    date = models.CharField(max_length=30, blank=True)
+    date = models.CharField(max_length=300, blank=True)
 
-    time = models.CharField(max_length=30 , blank=True)
+    time = models.CharField(max_length=300 , blank=True)
 
-    incident_location = models.TextField(max_length=100, blank=True)
+    incident_location = models.TextField(max_length=300, blank=True)
 
-    initial_severity = models.CharField(max_length=30, blank=True)
+    initial_severity = models.CharField(max_length=300, blank=True)
 
-    suspected_cause = models.TextField(max_length=100, blank=True)
+    suspected_cause = models.TextField(max_length=300, blank=True)
 
-    immediate_action_taken = models.TextField(max_length=100, blank=True)
+    immediate_action_taken = models.TextField(max_length=300, blank=True)
 
-    sub_incident_type = models.CharField(max_length=30, blank=True)
+    sub_incident_type = models.CharField(max_length=300, blank=True)
 
-    reporting_user = models.CharField(max_length=50, blank=True)
+    reporting_user = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
 
